d7c51fb07c87987b4d07b8793127417c8b617dbe
haroldCoder <haroldc2005@gmail.com>
2026-02-24 21:43:25 -0500

    refactor: remove PrismaService from DocumentModule providers.
    fix: error of connecting twice to the database

--- src/modules/document/document.module.ts
+++ src/modules/document/document.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { DocumentService } from "@document/infrastructure/document.service";
 import { DocumentController } from "@document/infrastructure/document.controller";
-import { PrismaService } from "@/common/infrastructure/db";
 
 @Module({
     controllers: [DocumentController],
-    providers: [DocumentService, PrismaService],
+    providers: [DocumentService],
     exports: [DocumentService]
 })
 export class DocumentModule { }
