3a673c6a1cc2d778b76d853eba91a337fe4dff3a
haroldCoder <haroldc2005@gmail.com>
2026-09-02 18:07:12 -0500

    feat: add pagination and sorting support to vaccination and appointment search DTOs

--- src/modules/appointment/presentation/dtos/find-appointment-criteria.dto.ts
+++ src/modules/appointment/presentation/dtos/find-appointment-criteria.dto.ts
@@ -1,9 +1,30 @@
 import { FindAppointmentsCriteria } from "@appointment/domain/ports/appointment.repository";
-import { IsOptional } from "class-validator";
+import { IsOptional, IsInt, Min, IsString, IsIn } from "class-validator";
 import { AppointmentStatus } from "@appointment/domain/enums/appointment-status.enum";
-import { Transform } from "class-transformer";
+import { Transform, Type } from "class-transformer";
 
 export class FindAppointmentsCriteriaDto implements FindAppointmentsCriteria {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    page?: number;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    limit?: number;
+
+    @IsOptional()
+    @IsString()
+    sortField?: string;
+
+    @IsOptional()
+    @IsIn(['asc', 'desc'])
+    sortOrder?: 'asc' | 'desc';
+
+
     @IsOptional()
     @Transform(({ value }) => typeof value === 'string' ? value.split(',') : value)
     status?: AppointmentStatus[];
--- src/modules/vaccination/presentation/dtos/find-vaccination-criteria.dto.ts
+++ src/modules/vaccination/presentation/dtos/find-vaccination-criteria.dto.ts
@@ -1,9 +1,29 @@
 import { VaccinationStatus } from "@vaccination/domain/enums";
 import { FindVaccinationsCriteria } from "@vaccination/domain/ports";
-import { Transform } from "class-transformer";
-import { IsOptional } from "class-validator";
+import { Transform, Type } from "class-transformer";
+import { IsInt, IsOptional, IsString, Min, IsIn } from 'class-validator';
 
 export class FindVaccinationsCriteriaDto implements FindVaccinationsCriteria {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    page?: number;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    limit?: number;
+
+    @IsOptional()
+    @IsString()
+    sortField?: string;
+
+    @IsOptional()
+    @IsIn(['asc', 'desc'])
+    sortOrder?: 'asc' | 'desc';
+
     @IsOptional()
     @Transform(({ value }) => typeof value === 'string' ? value.split(',') : value)
     status?: VaccinationStatus[];
