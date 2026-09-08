ce924859af86785f60ffdcec1d751e4f33c3548b
haroldCoder <haroldc2005@gmail.com>
2026-09-08 11:03:35 -0500

    feat: add status filtering support to appointment user search query

--- src/modules/appointment/infrastructure/persistence/prisma-appointment.service.ts
+++ src/modules/appointment/infrastructure/persistence/prisma-appointment.service.ts
@@ -60,7 +60,7 @@ export class PrismaAppointmentService implements IAppointmentRepository {
     }
 
     async findByUserId(userId: string, criteria?: FindAppointmentsCriteria): Promise<AppointmentRelationUserDto[]> {
-        const { startDate, endDate, sortOrder } = criteria || {};
+        const { startDate, endDate, sortOrder, status } = criteria || {};
 
         const normalizedStartDate = startDate ? normalizeStartDAte(startDate) : undefined;
         const normalizedEndDate = endDate ? normalizeEndDate(endDate) : undefined;
@@ -73,6 +73,7 @@ export class PrismaAppointmentService implements IAppointmentRepository {
                 ],
                 ...(normalizedStartDate && { date: { gte: normalizedStartDate } }),
                 ...(normalizedEndDate && { date: { lte: normalizedEndDate } }),
+                ...(status && status.length > 0 && { status: { in: status as AppointmentStatus[] } })
             },
             include: {
                 pet: { select: { id: true, name: true, mainImage: true, owner: { select: { user: { select: { name: true } } } } } },
