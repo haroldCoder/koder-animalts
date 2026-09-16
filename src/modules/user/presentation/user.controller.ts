36d218eb57ef2f8d6e012c6410af7877016e0ab7
haroldCoder <haroldc2005@gmail.com>
2026-09-16 11:05:47 -0500

    docs: add swagger annotations to user module

--- src/modules/user/presentation/user.controller.ts
+++ src/modules/user/presentation/user.controller.ts
@@ -1,12 +1,17 @@
+import { ApiTags, ApiOperation, ApiResponse, ApiProperty } from '@nestjs/swagger';
 import { ResponseDto } from "@/common/domain/dto";
 import { Controller, Get, Param } from "@nestjs/common";
 import { GetUserRoleUseCase } from "@user/application/use-cases";
 import { UserMetadata } from "@user/domain/ports";
 
+@ApiTags('User')
 @Controller('users')
 export class UserController {
     constructor(private readonly getUserRoleUseCase: GetUserRoleUseCase) { }
 
+    @ApiOperation({ summary: 'Get role' })
+    @ApiResponse({ status: 200, description: 'Operación exitosa.' })
+    @ApiResponse({ status: 400, description: 'Solicitud inválida.' })
     @Get(':id/role')
     async getRole(@Param('id') id: string): Promise<ResponseDto<UserMetadata>> {
         return this.getUserRoleUseCase.execute(id);
